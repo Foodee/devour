eb2a3bdcbc3937e627c2e7740c999bed2b63b554
Maz Ahmadi <maziar.ahmadi@icloud.com>
2017-01-16 12:46:25 -0800

    Fix pluralize bug in request method

--- src/middleware/json-api/req-create-relation.js
+++ src/middleware/json-api/req-create-relation.js
@@ -1,8 +1,10 @@
+const pluralize = require('pluralize')
+
 module.exports = {
   formatReqPayload: (obj) => {
     return JSON.stringify({
       data: {
-        type: obj.modelName,
+        type: pluralize.plural(obj.modelName),
         attributes: obj.attributes,
         relationships: obj.relationships
       }
@@ -12,8 +14,10 @@ module.exports = {
     return new Promise((resolve, reject) => {
       let xhr = new XMLHttpRequest();
       xhr.open(req.method, req.url)
+      xhr.setRequestHeader('Content-Type', 'application/vnd.api+json')
+      xhr.setRequestHeader('Accept', 'application/vnd.api+json')
       Object.keys(headers).forEach((key) => {
-        xhr.setRequestHeader(key.toString(), this.headers[key].toString())
+        xhr.setRequestHeader(key.toString(), headers[key].toString())
       })
       xhr.send(req.data)
 
